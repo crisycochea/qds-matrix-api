2f78ffe74a802de725f52bb353ce1c9ea27f24f3
Cristian Ycochea <crisycochea@gmail.com>
2023-02-27 18:27:48 -0500

    feat: cleaning the app from the starter

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,11 @@ import bodyParser from "body-parser";
 import express from "express";
 
 import { config } from "./shared/infrastructure/config";
-import { userRouter } from "./users/infrastructure/rest-api/user-router";
 
 function boostrap() {
   const app = express();
 
   app.use(bodyParser.json());
-  app.use("/users", userRouter);
 
   const { port } = config.server;
 
