acb361fdd706cb8052371708980a6cb84557ac9c
Cristian Ycochea <crisycochea@gmail.com>
2023-02-27 18:54:49 -0500

    feat: defining the tests and basic class for squareMatrix

---
+++ tests/matrix/application/squareMatrix.test.ts
@@ -0,0 +1,57 @@
+import NotSquareMatrix from "../../../src/matrix/domain/exceptions/NotSquareMatrix";
+import SquareMatrix from "../../../src/matrix/domain/models/squareMatrix";
+
+describe("RotateMatrix", () => {
+  describe("rotateEvenSizeMatrix", () => {
+    it("should throw error", async () => {
+      const matrixArray = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+        [0, 1, 2],
+      ];
+      expect(() => {
+        new SquareMatrix(matrixArray);
+      }).toThrowError(NotSquareMatrix);
+    });
+  });
+  describe("rotateEvenSizeMatrix", () => {
+    it("should return [[2,4],[1,3]]", async () => {
+      const matrixArray = [
+        [1, 2],
+        [3, 4],
+      ];
+      const matrix = new SquareMatrix(matrixArray);
+
+      const counterClockWiseRotatedMatrixArray = [
+        [2, 4],
+        [1, 3],
+      ];
+      matrix.rotateCounterClockWise();
+      expect(JSON.stringify(matrix.asArray())).toStrictEqual(
+        JSON.stringify(counterClockWiseRotatedMatrixArray)
+      );
+    });
+
+    describe("rotateEvenSizeMatrix", () => {
+      it("should return [ [3,6,9], [2,5,8], [1,4,7] ]", async () => {
+        const matrixArray = [
+          [1, 2, 3],
+          [4, 5, 6],
+          [7, 8, 9],
+        ];
+        const matrix = new SquareMatrix(matrixArray);
+
+        const counterClockWiseRotatedMatrixArray = [
+          [3, 6, 9],
+          [2, 5, 8],
+          [1, 4, 7],
+        ];
+        matrix.rotateCounterClockWise();
+        expect(JSON.stringify(matrix.asArray())).toStrictEqual(
+          JSON.stringify(counterClockWiseRotatedMatrixArray)
+        );
+      });
+    });
+  });
+});
