30b4e115bc5450d9c4cbd2cc8f14ecd21907ea9b
Cristian Ycochea <crisycochea@gmail.com>
2023-02-27 19:14:27 -0500

    feat: validating the matrix is square on constructor

--- src/matrix/domain/models/squareMatrix.ts
+++ src/matrix/domain/models/squareMatrix.ts
@@ -1,12 +1,22 @@
+import NotSquareMatrix from "../exceptions/NotSquareMatrix";
+
 export default class SquareMatrix {
-  matrix_array: Array<Array<number>>;
+  private matrix_array: Array<Array<number>>;
   constructor(matrix_array: Array<Array<number>>) {
+    const matrixLength = matrix_array.length;
+    const subListsWithSameLength = matrix_array.every(
+      (subList) => subList.length === matrixLength
+    );
+    if (!subListsWithSameLength) {
+      throw new NotSquareMatrix();
+    }
     this.matrix_array = matrix_array;
   }
 
   public rotateCounterClockWise() {
     this.matrix_array = [[1]];
   }
+
   // to encapsulate the matrix_array, and let the option to get in different formats
   // e.g asString, asList
   public asArray() {
